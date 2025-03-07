12c544fef2993ab363ccd5a3d3730045483a5312
donmatthiuz <cor22982@uvg.edu.gt>
2025-03-07 12:59:35 -0600

    feat Add of phtreads and config

--- Bounded/Bounded-Buffer.c
+++ Bounded/Bounded-Buffer.c
@@ -3,63 +3,100 @@
 #include <pthread.h>
 #include "buffer.h"
 
-#define MAX_ITEMS 10  
+#define TAMANO_BUFFER 10  
+#define PRODUCTORES 5
+#define CONSUMIDORES 5
+#define ESCRITURA 3  // Cantidad máxima de items que puede llegar a escribir un productor
 
 buffer_t buffer;
 pthread_mutex_t mutex;
 pthread_cond_t full, empty;
 
-void* producer(void* arg) {
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        slot_t item = {i + 1};
 
-        pthread_mutex_lock(&mutex);
-        while (buffer.size == buffer.capacity) {
+void* producer(void* arg) {
+    int producer_id = *((int*)arg);
+    int count = 0;
+
+    while (1) {  
+        slot_t item = {count + 1};  
+
+        pthread_mutex_lock(&mutex);  
+
+        while (buffer.size == buffer.capacity) { 
             pthread_cond_wait(&empty, &mutex);
         }
 
-        buffer_insert(&buffer, &item);
-        printf("Productor: %d\n", item.value);
-        buffer_dump(&buffer);
+        buffer_insert(&buffer, &item); 
+        printf("Productor %d: %d\n", producer_id, item.value);
+        buffer_dump(&buffer);  
 
         pthread_cond_signal(&full);
         pthread_mutex_unlock(&mutex);
+
+        count++; 
     }
+
     pthread_exit(NULL);
 }
 
+
 void* consumer(void* arg) {
-    for (int i = 0; i < MAX_ITEMS; i++) {
+    int consumer_id = *((int*)arg);
+    int count = 0;
+
+    while (1) { 
         slot_t item;
 
         pthread_mutex_lock(&mutex);
+
         while (buffer.size == 0) { 
             pthread_cond_wait(&full, &mutex);
         }
 
-        buffer_remove(&buffer, &item);
-        printf("Consumidor: %d\n", item.value);
-        buffer_dump(&buffer);
+        buffer_remove(&buffer, &item); 
+        printf("Consumidor %d: %d\n", consumer_id, item.value);
+        buffer_dump(&buffer);  
 
-        pthread_cond_signal(&empty);
-        pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&empty); 
+        pthread_mutex_unlock(&mutex); 
+
+        count++;  
     }
+
     pthread_exit(NULL);
 }
 
 int main() {
-    pthread_t producerThread, consumerThread;
+    pthread_t producerThreads[PRODUCTORES], consumerThreads[CONSUMIDORES];
+    int producer_ids[PRODUCTORES], consumer_ids[CONSUMIDORES];
 
-    buffer_init(&buffer, 5); 
+    buffer_init(&buffer, 5);
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&full, NULL);
     pthread_cond_init(&empty, NULL);
 
-    pthread_create(&producerThread, NULL, producer, NULL);
-    pthread_create(&consumerThread, NULL, consumer, NULL);
 
-    pthread_join(producerThread, NULL);
-    pthread_join(consumerThread, NULL);
+    for (int i = 0; i < PRODUCTORES; i++) {
+        producer_ids[i] = i + 1;
+        pthread_create(&producerThreads[i], NULL, producer, &producer_ids[i]);
+    }
+
+
+    for (int i = 0; i < CONSUMIDORES; i++) {
+        consumer_ids[i] = i + 1;
+        pthread_create(&consumerThreads[i], NULL, consumer, &consumer_ids[i]);
+    }
+
+
+    for (int i = 0; i < PRODUCTORES; i++) {
+        pthread_join(producerThreads[i], NULL);
+    }
+
+
+    for (int i = 0; i < CONSUMIDORES; i++) {
+        pthread_join(consumerThreads[i], NULL);
+    }
+
 
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&full);
