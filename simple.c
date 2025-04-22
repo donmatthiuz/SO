8aa813f3feb6ea340a137cf9b9b76ab5c430a7b9
Mathew Cordero <97419979+donmatthiuz@users.noreply.github.com>
2025-04-22 16:52:11 -0600

    Update simple.c

--- simple.c
+++ simple.c
@@ -3,20 +3,18 @@
 #include <linux/module.h>
 #include <linux/list.h>
 
-
 static int __init simple_init(void){
-    printk(KERN_INFO "Loading Module\nEstoy en el sistema operativo");
+    printk(KERN_INFO "Loading Module: Estoy en el sistema operativo\n");
     return 0;
 }
 
-
 static void __exit simple_exit(void){
-    printk(KERN_INFO "Removing Module\nEstoy afuera en el sistema operativo");
+    printk(KERN_INFO "Removing Module: Estoy afuera en el sistema operativo\n");
 }
 
 module_init(simple_init);
 module_exit(simple_exit);
-MODULE_LICENSE("GPL");
 
+MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Practica de Cargar y Descargar.");
 MODULE_AUTHOR("MATHEW CORDERO AQUINO - 22982");
